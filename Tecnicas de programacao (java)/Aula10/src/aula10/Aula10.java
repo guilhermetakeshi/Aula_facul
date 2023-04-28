5c29a610c7a7ba2a1b8bbe3e4c62026fb82213cb
Guilherme Takeshi Oshiro Andrade <126925552+guilhermetakeshi@users.noreply.github.com>
2023-04-27 21:18:18 -0300

    Add files via upload

---
+++ Tecnicas de programacao (java)/Aula10/src/aula10/Aula10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula10;
+
+/**
+ *
+ * @author autologon
+ */
+public class Aula10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tela1 t1 = new Tela1();
+        t1.setVisible(true);
+    }
+    
+}
