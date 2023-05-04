153dd59dc8ad3f2fcc950715f61f69fba9dd1235
Guilherme Takeshi Oshiro Andrade <126925552+guilhermetakeshi@users.noreply.github.com>
2023-05-04 20:21:33 -0300

    Add files via upload

---
+++ Tecnicas de programacao (java)/ExercicioAula0405.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicioaula04.pkg05;
+
+/**
+ *
+ * @author autologon
+ */
+public class ExercicioAula0405 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tela1 t1 = new Tela1();
+        t1.setVisible(true);
+    }
+    
+}
